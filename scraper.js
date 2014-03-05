f526b71a5baaaa41e222a8d758878112b46f6d1c
mjfuller <mfuller51@yahoo.com>
2014-03-05 16:30:47 -0500

    basic scraper sudocode

---
+++ scraper.js
@@ -0,0 +1,36 @@
+//SUDE CODE FOR SCAPER
+
+/*
+entry {
+	content
+	title
+	author
+	date
+	other stuff
+}
+*/
+
+//GET HTML FROM WEBPAGE
+function getHtml(var url) {
+	//return someString;
+}
+
+function getDOMFromHtml(var html) {
+	//return a DOM object
+}
+
+function getContent(var dom) {
+	//return some string
+}
+
+function getTitle(var dom) {
+	//return some string
+}
+
+function getAuthor(var dom) {
+	//return some string
+}
+
+function getDate(var dom) {
+	//return some string
+}
