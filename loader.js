c8626a74adc5e3b4e4d75c5929d62c775087fc35
Max Fuller <mjfuller@cs.brown.edu>
2014-03-18 21:09:08 -0400

    stuff

--- loader.js
+++ loader.js
@@ -1,18 +1,25 @@
 var anyDB = require('any-db');
-var conn = anyDB.createConnection('sqlite3://chatroom.db');
+var conn = anyDB.createConnection('sqlite3://digestible.db');
 
-conn.query('CREATE TABLE emails (email-id TEXT PRIMARY KEY, recipient TEXT, date-to-send TEXT, entry-id TEXT, collection-id TEXT);') 
+conn.query('CREATE TABLE Emails (email-id TEXT PRIMARY KEY, recipient TEXT, ' + 
+    'date-to-send TEXT, entry-id TEXT, collection-id TEXT);') 
     .on('error', function() {
     	console.error; 
     });
 
-conn.query('CREATE TABLE entries (entry-id TEXT PRIMARY KEY, collection-id TEXT, collection-title TEXT, ' + 
-	'creator-email TEXT, author TEXT, title TEXT, date-submitted TEXT, content TEXT);') 
+conn.query('CREATE TABLE Entries (entry-id TEXT PRIMARY KEY, collection-id TEXT, ' + 
+    'author TEXT, title TEXT, date-submitted TEXT, content TEXT);') 
     .on('error', function() {
     	console.error; 
     });
 
-con.query('CREATE TABLE creator-login (email TEXT PRIMARY KEY, password, TEXT)')
+conn.query('CREATE TABLE Collections (collection-id TEXT PRIMARY KEY, ' + 
+    'collection-title TEXT, creator-email TEXT);') 
+    .on('error', function() {
+        console.error; 
+    });
+
+conn.query('CREATE TABLE Creator-login (email TEXT PRIMARY KEY, password TEXT)')
 	.on('error', function() {
 		console.error;
 	});
@@ -31,14 +38,17 @@ con.query('CREATE TABLE creator-login (email TEXT PRIMARY KEY, password, TEXT)')
     //entry TABLE
     	//(primary) entry id
     	//collection id
-    	//collection title
-    	//creator email
     	//author
     	//title
     	//date-submitted
     	//content
     	//color scheme
     	//more data
+
+    //collections TABLE
+        //(primary) collection-id
+        //collection title
+        //creator email
 
     //username and password TABLE (for creators only)
     	//(primary) email
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require('http'),
 	express = require('express');
 var engines = require('consolidate');
 var anyDB = require('any-db');
-var conn = anyDB.createConnection('sqlite3://chatroom.db');
+var conn = anyDB.createConnection('sqlite3://digestible.db');
 var app = express();
 
 app.engine('html', engines.hogan); // tell Express to run .html files through Hogan
@@ -14,6 +14,18 @@ app.set('views', __dirname + '/templates'); // tell Express where to find templa
 app.use(express.bodyParser());
 app.use('/public', express.static(__dirname + '/public'));
 
+function Entry(entry-id, collection-id, collection-title, 
+    creator-email, author TEXT, title TEXT, date-submitted TEXT, content TEXT) {
+    this.entry-id = entry-id;
+    this.collection-id = collection-id;
+    this.collection-title = collection-title;
+    this.creator-email = creator-email;
+    this.author = author;
+    this.title = title;
+    this.date-submitted = date-submitted;
+    this.content = content;
+
+}
 //initi
 /* ////////////////////////////////////////////
 End of initialization
@@ -31,19 +43,37 @@ Methods that get called on our server
 *//////////////////////////////////////////////
 
 //generates an entry
-function generateNewEntry(roughEntry){
-    unique_id = generateMessageIdentifier();
+function makeNewEntry(entry, collection-id){
+    conn.query('INSERT INTO Entries (entry-id, collection-id, collection-title, ' + 
+        'creator-email, author, title, date-submitted, content)' + 
+        'VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
+        [
+            entry.entry-id, 
+            entry.collection-id, 
+            entry.collection-title,
+            entry.creator-email
+            entry.author,
+            entry.title,
+            entry.date-submitted,
+            entry.content
+        ])
+        .on('error', console.error);
 }
 
 //editEntry
-function editEntry(roughEntry, oldID){
+function editEntry(entry, entryID){
     unique_id = generateMessageIdentifier();
 }
 
 //deletes an entry
 function deleteEntry(entryID){
-
+    conn.query('DELETE FROM Entries WHERE entry-id=$1', [entryID])
+        .on('error', console.error);
 }
+
+function
+
+//
 
 //creates a subscription
 function subscribeToEmail(EntryId,whatotherparamtersdoweneed){
