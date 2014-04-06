3c5acbfd407d7677f489568effcb783fb21730c7
brresnic <benjamin_resnick@brown.edu>
2014-04-06 12:48:09 -0400

    restructuring

--- public/consumer.js
+++ public/consumer.js
@@ -1,10 +1,8 @@
 $(document).ready(function() { 
-     alert("aweffffff");
   $("#consumerForm").submit(function( event ) {
        
     // prevent the page from redirecting
     event.preventDefault();
-    alert("asdf");
     //send data to the server
     $.post("consumer",
     {
@@ -17,10 +15,3 @@ $(document).ready(function() {
     });
   });
 });
-
-/*//overrides default behavior of messageForm
-window.addEventListener('load', function(){
-   alert("awefa");
-    var messageForm = document.getElementById('signForm');
-    messageForm.addEventListener('submit', sendForm, false);
-}, false);*/
