0e604baf90b85e8009a017c7f8c6147597fce8cf
Pete Simpson <peter_simpson@brown.edu>
2014-04-13 15:20:37 -0400

    Finished settings overlay

--- public/clientjs/collection.js
+++ public/clientjs/collection.js
@@ -4,4 +4,15 @@ $(document).ready(function() {
     $('#noLinksPrompt').hide();
   }
 
+  $('#settingsHolder').click(function() {
+    $('#collSettingsOverlay').show();
+  });
+  $('#collOptionsForm .formSubmit').click(function() {
+    $('#collSettingsOverlay').hide();
+  });
+
+  $('#collEditForm .formSubmit').click(function() {
+    alert("This is just an example of what the the sign up form looks like for your readers.");
+  });
+
 });
